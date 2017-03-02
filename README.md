2701591870a012a25a85699afd133129061d6fc4
Malte311 <malte.luttermann@student.uni-luebeck.de>
2017-03-02 20:54:38 +0100

    gui.js kommentiert, updateRows() noch nicht korrekt

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Tetris
 
 TO-DO:
-Speed erhöhen sobald bestimmer Score erreicht wird (Levelsystem) neues Level Schriftzug
-updateRows funktioniert noch nicht korrekt?
-nächstes Objekt anzeigen, createnewObjectIsPossible auf das richtige Objekt anwenden
-Scoreanzeige und Play Again Button implementieren
-Bugreport Funktion?, Globale Scores?
-schnell runter , aber nicht komplett frei fallen, tasten gedrückt halten für mehrere aktionen?
+1.Problem bei updateRows() in Gui.js : Alle Steine, wo nichts drunter ist, fallen runter, auch wenn sie eigentlich in einem Feld daneben fest sind und nicht in der Luft hängen
+und es wird nur ein Mal durchlaufen von oben nach unten, also wenn etwas runter fällt und darüber was in der Luft hängt, rückt es nicht nach
+2.Scoreanzeige und Play Again Button implementieren, nächstes Objekt anzeigen, Help Button
+3.Speed erhöhen sobald bestimmer Score erreicht wird (Levelsystem) neues Level Schriftzug (am besten im Spielfeld kurz Einblenden)
+4.Tasten gedrückt halten für Bewegungen, schnell runter fallen lassen können ohne direkt komplett frei fallen zu lassen
+5.Bugreport Funktion?, Globale Scores?
--- gui.js
+++ gui.js
@@ -1,52 +1,72 @@
 //Konstrutor Funktion
 function GUI() {
-
   //Variablen vom Graphical User Interface
+  //Variable fuer den Scorewert
   this.score = 0;
+  //Die Breite eines Blockes
   this.blockBreite = 22;
+  //Die Hoehe eines Blockes (gleich der Breite, da Bloecke quadratisch sein sollen)
   this.blockHoehe = this.blockBreite;
+  //Die Anzahl der Bloecke pro Zeile
   this.bloeckeProZeile = 12;
+  //Die Anzahl der Bloecke pro Spalte
   this.bloeckeProSpalte = 20;
+  //Das Array welches das Spielfeld in Bloecken unterteilt speichert
   this.gridArray = [this.bloeckeProSpalte];
-
   //Funktion zum Zeichnen des Grids
   this.grid = function() {
-    //Grid zeichnen
+    //Grid zeichnen, Linien in weiß
     stroke(255);
+    //Es werden Rechtecke gezeichnet, die vorerst nicht ausgefuellt werden sollen
     noFill();
+    //Durchlaufe alle Reihen
     for (var i = 0; i < this.bloeckeProSpalte; i++) {
+      //Durchlaufe alle Felder einer Reihe
       for (var j = 0; j < this.bloeckeProZeile; j++) {
+        //Zeichne Rechtecke, so dass ein Grid zustande kommt
         rect(j * this.blockBreite, i * this.blockHoehe, this.blockBreite, this.blockHoehe);
       }
     }
   }
-
   //Funktion zum Fuellen des Arrays
   this.fillArray = function() {
     //Zweidimensionales Array erstellen
+    //Dazu Array durchlaufen
     for (var i = 0; i < this.bloeckeProSpalte; i++) {
+      //Und mit Arrays befuellen
       this.gridArray[i] = new Array(this.bloeckeProZeile);
     }
+    //Dann das soeben erstellte Array komplett durchlaufen
     for (var i = 0; i < this.bloeckeProSpalte; i++) {
       for (var j = 0; j < this.bloeckeProZeile; j++) {
+        //Und ueberall den Wert 0 speichern
         this.gridArray[i][j] = 0;
       }
     }
   }
   //Funktion zum Entfernen einer vollen Zeile
+  //Bekommt als Parameter den Index des Arrays uebergeben, der angibt, welche Zeile geloescht wird
   this.deleteVolleReihe = function(index) {
+    //Durchlaufe die zu loeschende Zeile
     for (var i = 0; i < graphics.bloeckeProZeile; i++) {
+      //Setze die Werte alle auf 0 zurueck
       graphics.gridArray[index][i] = 0;
+      //Die Reihen muessen geupdated werden, damit keine Steine in der Luft haengen
       this.updateRows();
+      //Der Score wird erhoeht, da eine volle Reihe zustande gekommen ist
       this.updateScore();
     }
   }
   //Funktion zum Zeichnen der Objekte
   this.drawObjects = function() {
+    //Durchlaufe das Array komplett
     for (var i = 0; i < this.bloeckeProSpalte; i++) {
       for (var j = 0; j < this.bloeckeProZeile; j++) {
+        //Sollte irgendwo ein Wert ungleich Null stehen, tue folgendes
         if (this.gridArray[i][j] != 0) {
           //Switch Statement fuer die Farbwahl
+          //Negative Werte bedeuten feste Steine
+          //Positive Werte bedeuten der Stein bewegt sich aktuell noch
           switch (this.gridArray[i][j]) {
             //Siebter Fall ist NormalesL (orange) (Stein bereits fest)
             case -7:
@@ -105,17 +125,24 @@ function GUI() {
               fill(255, 128, 0);
               break;
           }
+          //Die gezeichneten Rechtecke sollen schwarz umrandet gezeichnet werden
           stroke(0);
+          //Dann werden Rechtecke an der entsprechenden Position mit der entsprechenden Farbe gezeichnet
           rect(j * this.blockBreite, i * this.blockHoehe, this.blockBreite, this.blockHoehe);
         }
       }
     }
   }
   //Funktion zum Neuzeichnen des Spielfeldes
+  //Sorgt dafuer, dass der Stein, sobald er seine Position aendert, nur auf der neuen Position zu sehen ist
+  //Und die alte Position rausgenommen wird
   this.repaintField = function() {
+    //Durchlaufe das Array komplett
     for (var i = 0; i < this.bloeckeProSpalte; i++) {
       for (var j = 0; j < this.bloeckeProZeile; j++) {
+        //Wenn ein positiver Wert gefunden wird, setze ihn direkt zurueck
         if (this.gridArray[i][j] > 0) {
+          //Setze Wert wieder auf Null
           this.gridArray[i][j] = 0;
         }
       }
@@ -130,18 +157,22 @@ function GUI() {
   }
   //Funktion zum Updaten der Scoreanzeige
   this.updateScore = function() {
+    //Fuer jeden Stein, der entfernt wird, gibt es einen Punkt
     this.score += 1;
     //Score in der Console ausgeben
     console.log(this.score);
   }
   //Funktion zum Runterfallen von Steinen, die in der Luft schweben wuerden, wenn eine Reihe geloescht wird
-
-  //funktioniert noch nicht korrekt
   this.updateRows = function() {
+    //Durchlaufe alle Reihen, bis auf die unterste (daher -1)
     for (var i = 0; i < graphics.bloeckeProSpalte - 1; i++) {
+      //Durchlaufe alle Felder der Reihe
       for (j = 0; j < graphics.bloeckeProZeile; j++) {
+        //Wenn ein Feld einen Stein enthaelt, das darunter liegende Feld aber nicht, dann tue folgendes
         if (graphics.gridArray[i][j] < 0 && graphics.gridArray[i + 1][j] == 0) {
+          //Das Feld darunter enthaelt nun den Stein
           graphics.gridArray[i + 1][j] = graphics.gridArray[i][j];
+          //Das Feld darueber wird auf Null zurueckgesetzt
           graphics.gridArray[i][j] = 0;
         }
       }
