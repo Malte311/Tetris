46651f705519753369bb355a79ab44bb7e734511
Malte311 <malte.luttermann@student.uni-luebeck.de>
2017-03-03 21:23:55 +0100

    normalesI.js bis inklusive freierFall() kommentiert und korrigiert.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 TO-DO:
 1.Problem bei updateRows() in Gui.js : Alle Steine, wo nichts drunter ist, fallen runter, auch wenn sie eigentlich in einem Feld daneben fest sind und nicht in der Luft hängen
 und es wird nur ein Mal durchlaufen von oben nach unten, also wenn etwas runter fällt und darüber was in der Luft hängt, rückt es nicht nach
-2.Scoreanzeige und Play Again Button implementieren, nächstes Objekt anzeigen, Help Button
+2.Scoreanzeige und Play Again Button implementieren, nächstes Objekt anzeigen, Help Button (jQuery-UI & jQuery-UI Tooltip)
 3.Speed erhöhen sobald bestimmer Score erreicht wird (Levelsystem) neues Level Schriftzug (am besten im Spielfeld kurz Einblenden)
 4.Tasten gedrückt halten für Bewegungen, schnell runter fallen lassen können ohne direkt komplett frei fallen zu lassen
 5.Bugreport Funktion?, Globale Scores?
--- objects/normalesI.js
+++ objects/normalesI.js
@@ -1,11 +1,17 @@
 //Konstruktor Funktion
 function NormalesI() {
   //Position bestehend aus x Wert und y Wert
+  //Zu Anfang in der Mitte des Feldes
   this.x = graphics.bloeckeProZeile / 2 - 1;
+  //und ganz oben
   this.y = 0;
+  //farbCode fuer diesen Stein waehrend er sich bewegt
   this.farbCode = 2;
+  //farbCode sobald der Stein sich nicht weiter bewegt
   this.platziert = -2;
+  //zu Anfang bewegt er sich
   this.isMoving = true;
+  //und ist zu Anfang senkrecht
   this.senkrecht = true;
   //Groesse eines Objektes in Bloecken gemessen
   this.hoehe =  4;
@@ -15,7 +21,9 @@ function NormalesI() {
     //Wenn der Stein senkrecht ist
     if (this.senkrecht) {
       //bewegt sich der Block noch, wird dies ausgefuehrt
-      if (this.isMoving && this.y + this.hoehe < graphics.bloeckeProSpalte) {
+      //Ist der Block ganz unten, bewegt er sich nicht weiter
+      if (this.isMoving && round(this.y) + this.hoehe < graphics.bloeckeProSpalte) {
+        //Das Objekt besteht aus vier kleinen untereinander gereihten Rechtecken
         graphics.gridArray[round(this.y)][this.x] = this.farbCode;
         graphics.gridArray[round(this.y) + 1][this.x] = this.farbCode;
         graphics.gridArray[round(this.y) + 2][this.x] = this.farbCode;
@@ -23,81 +31,117 @@ function NormalesI() {
       }
       //bewegt sich der Block nicht mehr oder kommt ganz unten an wird dies ausgefuehrt
       else {
+        //Das Objekt besteht aus vier kleinen untereinander gereihten Rechtecken
         graphics.gridArray[round(this.y)][this.x] = this.platziert;
         graphics.gridArray[round(this.y) + 1][this.x] = this.platziert;
         graphics.gridArray[round(this.y) + 2][this.x] = this.platziert;
         graphics.gridArray[round(this.y) + 3][this.x] = this.platziert;
+        //Und nun bewegt sich das Objekt nicht weiter
         this.isMoving = false;
       }
     }
     //Wenn der Stein quer liegt
     else {
       //bewegt sich der Block noch, wird dies ausgefuehrt
-      if (this.isMoving && this.y + this.hoehe < graphics.bloeckeProSpalte) {
-        graphics.gridArray[floor(this.y + 1)][this.x] = this.farbCode;
-        graphics.gridArray[floor(this.y + 1)][this.x + 1] = this.farbCode;
-        graphics.gridArray[floor(this.y + 1)][this.x + 2] = this.farbCode;
-        graphics.gridArray[floor(this.y + 1)][this.x + 3] = this.farbCode;
+      if (this.isMoving && round(this.y) + this.hoehe < graphics.bloeckeProSpalte) {
+        //Das Objekt besteht aus vier kleinen hintereinander gereihten Rechtecken
+        graphics.gridArray[round(this.y + 1)][this.x] = this.farbCode;
+        graphics.gridArray[round(this.y + 1)][this.x + 1] = this.farbCode;
+        graphics.gridArray[round(this.y + 1)][this.x + 2] = this.farbCode;
+        graphics.gridArray[round(this.y + 1)][this.x + 3] = this.farbCode;
       }
+      //bewegt sich der Block nicht mehr oder kommt ganz unten an wird dies ausgefuehrt
       else {
-        //bewegt sich der Block nicht mehr oder kommt ganz unten an wird dies ausgefuehrt
-        graphics.gridArray[floor(this.y)][this.x] = this.platziert;
-        graphics.gridArray[floor(this.y)][this.x + 1] = this.platziert;
-        graphics.gridArray[floor(this.y)][this.x + 2] = this.platziert;
-        graphics.gridArray[floor(this.y)][this.x + 3] = this.platziert;
+        //Das Objekt besteht aus vier kleinen hintereinander gereihten Rechtecken
+        graphics.gridArray[round(this.y + 1)][this.x] = this.platziert;
+        graphics.gridArray[round(this.y + 1)][this.x + 1] = this.platziert;
+        graphics.gridArray[round(this.y + 1)][this.x + 2] = this.platziert;
+        graphics.gridArray[round(this.y + 1)][this.x + 3] = this.platziert;
+        //Und das Objekt bewegt sich nicht weiter
         this.isMoving = false;
       }
     }
   }
   //Funktion zum Drehen
   this.drehen = function() {
+    //Variable zur Hilfe, um Breite und Hoehe tauschen zu koennen, sobald gedreht wird
     var tauschen = this.hoehe;
     //Wenn es senkrecht ist, wird es quer gedreht
     if (this.senkrecht) {
-      if (graphics.gridArray[round(this.y)][this.x - 2] == 0 && graphics.gridArray[round(this.y)][this.x - 1] == 0 &&
-      graphics.gridArray[round(this.y)][this.x + 1] == 0) {
-        this.x = this.x - 2;
-        this.y = this.y + 1;
-        this.hoehe = this.breite;
-        this.breite = tauschen;
-        this.senkrecht = !this.senkrecht;
+      //Um drehen zu koennen, muss erst geprueft werden, ob ausreichend Platz vorhanden ist
+      //Dafuer muss erst mal sichergestellt werden, dass das Array nicht verlassen wird, sprich, dass nur innerhalb des Feldes geprueft wird
+      if (this.x > 1 && round(this.y) < graphics.bloeckeProSpalte - 1) {
+        if (graphics.gridArray[round(this.y + 1)][this.x - 2] == 0 && graphics.gridArray[round(this.y + 1)][this.x - 1] == 0
+        && graphics.gridArray[round(this.y + 1)][this.x + 1] == 0) {
+          //Da der Stein nun nicht mehr senkrecht ist, soll seine Position nun angepasst werden
+          this.x = this.x - 2;
+          this.y = this.y + 1;
+          //Die Hoehe wird zur Breite
+          this.hoehe = this.breite;
+          //Die Breite wird zur Hoehe
+          this.breite = tauschen;
+          //Nachdem gedreht wurde, ist das Objekt nun nicht mehr senkrecht
+          this.senkrecht = !this.senkrecht;
+        }
       }
     }
     //Wenn es quer liegt wird es senkrecht gedreht
     else {
-      if (graphics.gridArray[round(this.y + 1)][this.x + 2] == 0 && graphics.gridArray[round(this.y + 2)][this.x + 2] == 0) {
-        this.x = this.x + 2;
-        this.y = this.y - 1;
-        this.hoehe = this.breite;
-        this.breite = tauschen;
-        this.senkrecht = !this.senkrecht;
+      //Auch hier wird erst geprueft, ob Platz zum Drehen ist und vorher sichergestellt, dass das Array nicht verlassen wird
+      if (this.x < graphics.bloeckeProZeile - 2 && round(this.y) < graphics.bloeckeProSpalte - 2) {
+        if (graphics.gridArray[round(this.y - 1)][this.x + 2] == 0 && graphics.gridArray[round(this.y + 1)][this.x + 2] == 0
+        && graphics.gridArray[round(this.y + 2)][this.x + 2] == 0) {
+          //Position des Objektes anpassen
+          this.x = this.x + 2;
+          this.y = this.y - 1;
+          //Hoehe und Breite vertauschen
+          this.hoehe = this.breite;
+          this.breite = tauschen;
+          //Nun ist das Objekt wieder senkrecht
+          this.senkrecht = !this.senkrecht;
+        }
       }
     }
   }
   //Funktion zum schnellen Fallenlassen eines Objektes
   this.freierFall = function() {
+    //Zunaechst einmal werden alle Felder einer Spalte durchlaufen
     for (var i = floor(this.y); i < graphics.bloeckeProSpalte; i++) {
       //wenn der Stein senkrecht steht
       if (this.senkrecht) {
+        //pruefen, ob an einer Stelle bereits ein Stein im Weg ist
         if (graphics.gridArray[i][this.x] < 0) {
+          //wenn ja, dann wird dieses Objekt direkt darüber platziert
           this.y = i - this.hoehe;
+          //und es bewegt sich nicht weiter
           this.isMoving = false;
+          //zudem muss die Schleife nicht weiter durchlaufen
           break;
         }
+        //Wenn nein, dann kann das Objekt nach ganz unten fallen
         else {
+          //Objekt ganz unten platzieren
           this.y = graphics.bloeckeProSpalte - this.hoehe;
+          //Objekt bewegt sich dann auch nicht mehr weiter
           this.isMoving = false;
         }
       }
       //Wenn der Stein quer liegt
       else {
+        //Zuerst wird auch wieder geprueft, ob irgendwo ein Hindernis auftaucht
         if (graphics.gridArray[i][this.x] < 0 || graphics.gridArray[i][this.x + 1] || graphics.gridArray[i][this.x + 2] || graphics.gridArray[i][this.x + 3]) {
+          //Wenn ja, wird dieses Objekt wieder direkt darueber platziert
           this.y = i - this.hoehe;
+          //Und es bewegt sich nicht mehr weiter
           this.isMoving = false;
+          //Zusaetzlich muss die Schleife nicht weiter durchlaufen
           break;
         }
+        //Wenn es kein Hindernis gibt, dann wird dies ausgefuehrt
         else {
+          //Das Objekt wird ganz unten platziert
           this.y = graphics.bloeckeProSpalte - this.hoehe;
+          //Und bewegt sich nicht weiter
           this.isMoving = false;
         }
       }
@@ -107,7 +151,9 @@ function NormalesI() {
   this.gravity = function() {
     //Wenn der Stein senkrecht ist
     if (this.senkrecht) {
-      if (this.y < graphics.bloeckeProSpalte - this.hoehe) {
+      //Das Objekt soll sinken, wenn es noch nicht ganz unten ist
+      if (round(this.y) < graphics.bloeckeProSpalte - this.hoehe) {
+        //Und wenn zusaetzlich kein Feld unter dem Objekt bereits besetzt ist
         if (graphics.gridArray[floor(this.y + this.hoehe)][this.x] == 0) {
           this.y += speed;
         }
@@ -118,9 +164,9 @@ function NormalesI() {
     }
     //Wenn der Stein quer liegt
     else {
-      if (this.y < graphics.bloeckeProSpalte - this.hoehe) {
-        if (graphics.gridArray[floor(this.y + this.hoehe)][this.x] == 0 && graphics.gridArray[floor(this.y + this.hoehe)][this.x + 1] == 0 &&
-        graphics.gridArray[floor(this.y + this.hoehe)][this.x + 2] == 0 && graphics.gridArray[floor(this.y + this.hoehe)][this.x + 3] == 0) {
+      if (round(this.y) < graphics.bloeckeProSpalte - this.hoehe) {
+        if (graphics.gridArray[round(this.y + this.hoehe)][this.x] == 0 && graphics.gridArray[round(this.y + this.hoehe)][this.x + 1] == 0 &&
+        graphics.gridArray[round(this.y + this.hoehe)][this.x + 2] == 0 && graphics.gridArray[round(this.y + this.hoehe)][this.x + 3] == 0) {
           this.y += speed;
         }
         else {
